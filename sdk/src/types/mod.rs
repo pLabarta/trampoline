033407bc34f6ba8a9cc5c4a13222a55a246ec669
Tannr <tannr@hey.com>
2022-04-05 14:00:50 -0400

    Add base trait for all types in types lib

--- sdk/src/types/mod.rs
+++ sdk/src/types/mod.rs
@@ -1,5 +1,21 @@
+use ckb_types::core::Capacity;
+use ckb_types::bytes::Bytes as CkBytes;
+use ckb_types::packed::Bytes as PackedBytes;
+use ckb_jsonrpc_types::JsonBytes;
+
 pub mod transaction;
 pub mod cell;
 pub mod script;
 pub mod bytes;
 pub mod constants;
+
+// TO DO: Implement this trait for all types
+
+pub trait TrampolineBaseType: Into<CkBytes> + Into<PackedBytes> + Into<JsonBytes> {
+    type Error: std::error::Error;
+    fn validate(&self) -> Result<(), Self::Error>;
+
+    fn required_capacity(&self) -> Result<Capacity, Self::Error>;
+
+    fn size_bytes(&self) -> Result<usize, Self::Error>;
+}
