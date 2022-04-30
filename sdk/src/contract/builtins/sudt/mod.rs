2c80ecfe29c526f812e4cfad7860c42329a6d48b
Tannr <tannr@hey.com>
2022-04-30 00:04:06 -0400

    bump ckb-* deps to v0.103.0

--- sdk/src/contract/builtins/sudt/mod.rs
+++ sdk/src/contract/builtins/sudt/mod.rs
@@ -1,5 +1,7 @@
 use std::prelude::v1::*;
 
+use ckb_types::H256;
+
 use crate::ckb_types::packed::{Byte32, Uint128};
 use crate::contract::{Contract, TContract};
 
@@ -11,7 +13,7 @@ struct InnerOwnerLockHash([u8; 32]);
 #[derive(Debug, Clone, Default)]
 struct InnerSudtAmount(u128);
 
-pub type OwnerLockHash = SchemaPrimitiveType<[u8; 32], Byte32>;
+pub type OwnerLockHash = SchemaPrimitiveType<H256, Byte32>;
 pub type SudtAmount = SchemaPrimitiveType<u128, Uint128>;
 
 impl TrampolineSchema for OwnerLockHash {}
