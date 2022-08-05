d10e76a5753230846626904f1f238287eb3f225b
plabarta <pablitolabarta@gmail.com>
2022-08-05 11:37:37 -0300

    docs: document Chain trait

--- sdk/src/chain/error.rs
+++ sdk/src/chain/error.rs
@@ -7,42 +7,55 @@ use thiserror::Error;
 use crate::types::cell::Cell;
 
 use super::CellInputs;
+
+/// Error types for Chain methods
 #[derive(Debug, Error)]
 pub enum ChainError {
+    /// Malformed transaction
     #[error(transparent)]
     TransactionError(#[from] TransactionError),
+    /// Failed to verify transaction
     #[error("Cannot verify transaction")]
     TransactionVerificationError,
+    /// Failed to publish transaction to the network
     #[error("Failed to send transaction to network")]
     TransactionSendError,
+    /// Failed to connect to node via RPC
     #[error("Failed to connect to node via RPC")]
     RpcError(#[from] RpcError),
+    /// Failed ton get transaction from node
     #[error("Failed to get Transaction from RPC")]
     GetTransactionError(H256),
+    /// Transaction not included in any block
     #[error("Transaction with hash {0}not included in any block yet")]
     TransactionNotIncluded(H256),
+    /// Block not found in chain
     #[error("Transaction with hash {0} not included in any block yet")]
     BlockNotFound(H256),
+    /// Block with number has not been mined yet
     #[error("Block with number {0} was not mined yet")]
     BlockNumberNotFound(u64),
+    /// Failed to resolve transaction due to outpoint error
     #[error("Failed resolving transaction due to outpoint error")]
     TxResolveError(#[from] OutPointError),
+    /// Genesis block not found
     #[error("Genesis block not found in chain, check your chain setup")]
     GenesisBlockNotFound,
-    #[error("Failed to connect to node via RPC")]
-    RpcConnectionError,
-    #[error("Selected lockscript not found in chain, make sure it is deployed")]
-    LockScriptCellNotFound(Cell),
+    /// Failed to get cell with specified outpoint
     #[error("Cell with outpoint {0} make sure it is deployed")]
     CellNotFound(OutPoint),
+    /// Failed to unlock transaction
     #[error("Failed to unlock transaction")]
     TransactionUnlockError(#[from] UnlockError),
+    /// Transaction cannot be deployed, still has unlocked inputs
     #[error("Failed to unlock deploy cell transaction, it has extra lock groups")]
     DeployCellTxHasLockedGroups,
+    /// Transaction has invalid cell inputs
     #[error("Failed to deploy transaction due to invalid cell inputs")]
     InvalidInputs(CellInputs),
 }
 
 // Most of this is taken from https://github.com/nervosnetwork/ckb-tool.
 // Reimplementation here due to slight changes in the API & version conflicts
+/// Result type for Chain methods
 pub type ChainResult<T> = Result<T, ChainError>;
--- sdk/src/chain/traits.rs
+++ sdk/src/chain/traits.rs
@@ -9,11 +9,15 @@ use crate::ckb_types::{
     packed::{Byte32, OutPoint},
 };
 
+/// Collections of scripts and their unlockers for signing transactions
 pub type Unlockers = HashMap<ScriptId, Box<dyn ScriptUnlocker>>;
 
+/// Variants for specifing cell inputs to be consumed by deploy_cell methods
 #[derive(Debug, Clone)]
 pub enum CellInputs {
+    /// Match cells with specified script
     ScriptQuery(Script),
+    /// Run a Chain's default cell collection to balance the transaction
     Empty,
 }
 
@@ -35,9 +39,14 @@ pub trait Chain {
     // Modify trait TransactionProvider to be more flexible about input type
     // Then define TransactionProviderError to use in Chain trait
     // This way, anything that accepts TransactionProvider trait can accept chain trait
+
+    /// Inner type that provides transactions verification and sending methods
     type Inner: TransactionProvider;
 
+    /// Returns the inner Transaction Prpvider
     fn inner(&self) -> Self::Inner;
+
+    /// Verify if a transaction is valid given the current chain state
     fn verify_tx<T: Into<TransactionView> + Clone>(&self, tx: T) -> ChainResult<T> {
         let view_tx: TransactionView = tx.clone().into();
         let json_tx = JsonTransaction::from(view_tx);
@@ -48,6 +57,7 @@ pub trait Chain {
         }
     }
 
+    /// Publish a transaction the the chain, returns its hash
     fn send_tx<T: Into<TransactionView> + Clone>(&self, tx: T) -> ChainResult<Byte32> {
         let view_tx: TransactionView = tx.into();
         let json_tx = JsonTransaction::from(view_tx);
@@ -57,12 +67,21 @@ pub trait Chain {
         }
     }
 
+    /// Deploys a Cell to the chain
+    ///
+    /// Cell Inputs must be provided, but if empty
+    /// cells from the Unlocker address will be used.
     fn deploy_cell(
         &mut self,
         cell: &Cell,
         unlockers: Unlockers,
         inputs: &CellInputs,
     ) -> ChainResult<OutPoint>;
+
+    /// Deploys multiples Cells to the chain
+    ///
+    /// Cell Inputs must be provided, but if empty
+    /// cells from the Unlocker address will be used.
     fn deploy_cells(
         &mut self,
         cells: &[Cell],
@@ -70,7 +89,9 @@ pub trait Chain {
         inputs: &CellInputs,
     ) -> ChainResult<Vec<OutPoint>>;
 
+    /// Changes the chain default lock
     fn set_default_lock(&mut self, lock: OutPoint) -> Result<(), ChainError>;
 
+    /// Creates a default cell using the Chain's default lock
     fn generate_cell_with_default_lock(&self, lock_args: Bytes) -> Cell;
 }
